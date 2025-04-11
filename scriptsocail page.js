44d68c67d5075b8b29b5b91d06a05637244f633f
testgithub292 <139734979+testgithub292@users.noreply.github.com>
2025-04-11 23:37:11 +0500

    add page

---
+++ scriptsocail page.js
@@ -0,0 +1,14 @@
+
+  // Get the navbar
+  const navbar = document.querySelector('.navbar');
+
+  // Listen for scroll event
+  window.addEventListener('scroll', function() {
+    if (window.scrollY >= 100) {
+      navbar.classList.add('scrolled');
+      navbar.classList.add('hidden');
+    } else {
+      navbar.classList.remove('scrolled');
+      navbar.classList.remove('hidden');
+    }
+  });
