26e79c9bf37e865b21b928555de9933c861d5cad
testgithub292 <139734979+testgithub292@users.noreply.github.com>
2025-05-19 00:34:41 +0500

    remove files

--- scripthomepage.js
+++ scripthomepage.js
@@ -895,11 +895,12 @@ function prepageCloseFullscreen() {
 
 */
 /*-------------------------*/
+/*
 function openImage() {
     var imageUrl = "https://drive.google.com/file/d/1VBN3NQNqYjBOpxB5cLDlbQR_2TtpjT3g/view?usp=sharing"; // Apni image ka URL yahan dalain
     window.open(imageUrl, "_blank");
 }
-
+*/
 
 
 /*---------------------------------------------------
@@ -1190,30 +1191,3 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(() => console.log('Service Worker Registered'));
-  }
-
-  let deferredPrompt;
-
-  window.addEventListener('beforeinstallprompt', (e) => {
-    e.preventDefault();
-    deferredPrompt = e;
-
-    const installBtn = document.getElementById('installBtn');
-    installBtn.style.display = 'block';
-
-    installBtn.addEventListener('click', () => {
-      installBtn.style.display = 'none';
-      deferredPrompt.prompt();
-      deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the install prompt');
-        } else {
-          console.log('User dismissed the install prompt');
-        }
-        deferredPrompt = null;
-      });
-    });
-  });
--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', function (event) {
-  console.log('Service Worker installed.');
-});
-
-self.addEventListener('fetch', function (event) {
-  // Simple cache strategy or skip this part
-});
